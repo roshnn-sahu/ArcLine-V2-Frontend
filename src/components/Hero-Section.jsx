f92191473394eab0bd38ece4db1c2cc1fe3a77c4
codewithroshann <locroshan@gmail.com>
2025-12-29 22:37:28 +0530

    feat: add Spline3D component and integrate with Hero section; update styles and dependencies

--- src/components/Hero-Section.jsx
+++ src/components/Hero-Section.jsx
@@ -1,12 +1,15 @@
-import Container from "../components/Container.jsx";
 import React from "react";
+import Container from "../components/Container.jsx";
+import GetStartedBtn from "./ui/GetStartedBtn.jsx";
+import Spline3D from "./ui/Spline3D.jsx";
 
 const Hero_Section = () => {
   return (
-    <>
-      <Container className=" px-6 selection:bg-secondary selection:text-white dark:selection:bg-primary">
-        <div className=" mb-6 pt-26 lg:pt-24">
-          <span className="text-sm font-semibold font-space">
+    <section className="relative  ">
+      {/* Content */}
+      <Container className=" grid grid-cols-1  lg:grid-cols-2 relative z-10 px-6 pt-28 pb-16 ">
+        <div>
+          <span className="text-sm font-semibold font-space flex items-center justify-center lg:justify-start">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -14,126 +17,41 @@ const Hero_Section = () => {
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="inline text-secondary mr-2 icon icon-tabler icons-tabler-outline icon-tabler-hand-three-fingers"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="icon icon-tabler icons-tabler-outline icon-tabler-hand-stop text-secondary mr-2"
             >
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M8 13v-8.5a1.5 1.5 0 0 1 3 0v7.5" />
-              <path d="M17 11.5a1.5 1.5 0 0 1 3 0v4.5a6 6 0 0 1 -6 6h-2h.208a6 6 0 0 1 -5.012 -2.7a69.74 69.74 0 0 1 -.196 -.3c-.312 -.479 -1.407 -2.388 -3.286 -5.728a1.5 1.5 0 0 1 .536 -2.022a1.867 1.867 0 0 1 2.28 .28l1.47 1.47" />
+              <path d="M8 13v-7.5a1.5 1.5 0 0 1 3 0v6.5" />
               <path d="M11 5.5v-2a1.5 1.5 0 1 1 3 0v8.5" />
               <path d="M14 5.5a1.5 1.5 0 0 1 3 0v6.5" />
+              <path d="M17 7.5a1.5 1.5 0 0 1 3 0v8.5a6 6 0 0 1 -6 6h-2h.208a6 6 0 0 1 -5.012 -2.7a69.74 69.74 0 0 1 -.196 -.3c-.312 -.479 -1.407 -2.388 -3.286 -5.728a1.5 1.5 0 0 1 .536 -2.022a1.867 1.867 0 0 1 2.28 .28l1.47 1.47" />
             </svg>
             We design. We build. We scale.
           </span>
 
-          <h1 className="text-6xl md:text-6xl  lg:text-7xl font-zalando w-full sm:w-[85%] md:w-[80%] xl:w-[80%] font-medium mb-10 mt-4 text-shadow-sm">
-            Turning{" "}
-            <span className="text-secondary">idea's</span>{" "}
-            into
-            <span className="text-secondary"> digital</span>{" "}
-            products
+          <h1 className="text-4xl md:text-6xl  font-zalando font-semibold max-w-4xl mt-6 mb-8  tracking-tight text-center lg:text-start">
+            Turning <span className="text-secondary">idea&apos;s</span> into{" "}
+            <span className="text-secondary">digital</span> products
           </h1>
-          <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-4 items-center my-12 ">
-            <span className="h-[1px] w-full bg-zinc-500"></span>
-            <p className="text-zinc-700 text-sm">
-            We build end-to-end digital solutions—from strategy and development to scalable, user-focused experiences that drive real business growth.
-            </p>
-          </div>
-          <div className="my-6 space-x-4 uppercase text-sm">
-            <a
-              href=""
-              className="text-zinc-600 hover:text-secondary transition-all ease-in-out duration-150 "
-            >
-              Linkedin
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="17"
-                height="17"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className=" rotate-270 inline  icon icon-tabler icons-tabler-outline icon-tabler-arrow-down-right"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M7 7l10 10" />
-                <path d="M17 8l0 9l-9 0" />
-              </svg>
-            </a>
-            <a
-              href=""
-              className="text-zinc-600 hover:text-secondary transition-all ease-in-out duration-150"
-            >
-              Github
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="17"
-                height="17"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className=" rotate-270 inline  icon icon-tabler icons-tabler-outline icon-tabler-arrow-down-right"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M7 7l10 10" />
-                <path d="M17 8l0 9l-9 0" />
-              </svg>
-            </a>
-            <a
-              href=""
-              className="text-zinc-600 hover:text-secondary transition-all ease-in-out duration-150"
-            >
-              Instragram
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="17"
-                height="17"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className=" rotate-270 inline  icon icon-tabler icons-tabler-outline icon-tabler-arrow-down-right"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M7 7l10 10" />
-                <path d="M17 8l0 9l-9 0" />
-              </svg>
-            </a>
-            <a
-              href=""
-              className="text-zinc-600 hover:text-secondary transition-all ease-in-out duration-150"
-            >
-              Email
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="17"
-                height="17"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className=" rotate-270 inline  icon icon-tabler icons-tabler-outline icon-tabler-arrow-down-right"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M7 7l10 10" />
-                <path d="M17 8l0 9l-9 0" />
-              </svg>
-            </a>
+
+          <p className="text-zinc-700 max-w-xl text-sm mb-6 text-center lg:text-start">
+            We build end-to-end digital solutions—from strategy and development
+            to scalable, user-focused experiences that drive real business
+            growth.
+          </p>
+          <div className="w-full text-start">
+
+          <GetStartedBtn />
           </div>
         </div>
+
+        <div className="">
+          <Spline3D />
+        </div>
       </Container>
-    </>
+    </section>
   );
 };
 
---
+++ src/components/ui/Spline3D.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import Spline from "@splinetool/react-spline";
+export default function Spline3D() {
+  function onLoad(spline) {
+    console.log("Spline loaded", spline);
+  }
+
+  function onMouseDown(e) {
+    console.log("Clicked object:", e.target.name);
+  }
+
+  return (
+    <div className="h-full w-full mask-x-from-60% mask-x-to-90% mask-y-from-60% mask-y-to-90%">
+      <Spline
+        style={{
+          height: "100%",
+          width: "100%",
+        }}
+        className=" overflow-none spline-container object-center "
+        scene="https://prod.spline.design/q2JiBhhjiJgHWP3N/scene.splinecode"
+        onLoad={onLoad}
+        onMouseDown={onMouseDown}
+      />
+    </div>
+  );
+}
