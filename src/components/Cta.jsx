8a1115ea7241e7d8f38fbb7363ef841daa2e7ca8
codewithroshann <locroshan@gmail.com>
2026-01-23 20:56:59 +0530

    fix: correct background position in Cta component for improved visual consistency

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -5,7 +5,7 @@ import Badge from "./ui/Badge";
 const Cta = ({ children, paragraph, badge = "Available For Work" }) => {
   return (
     <Container className="mb-6 px-4">
-      <div className="relative w-full overflow-hidden rounded-xl bg-cover bg-bottom bg-no-repeat shadow-lg [background:url('/cta-bg.png')]">
+      <div className="relative w-full overflow-hidden rounded-xl bg-cover bg-top bg-no-repeat shadow-lg bg-[url('/cta-bg.png')]">
         <div className="text-background relative z-[5] flex flex-col items-center justify-center px-4 py-10">
           <Badge text="text-white">{badge}</Badge>
 
